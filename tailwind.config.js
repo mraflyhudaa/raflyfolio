1176b3c4371642e844e98167a44c5c228e299858
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-01-28 23:17:25 +0700

    style: add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,45 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './modules/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'transform-text': 'change-text 6s infinite',
+      },
+      keyframes: {
+        'change-text': {
+          '35%': {
+            transform: 'translateY(-122%)',
+          },
+          '65%': {
+            transform: 'translateY(-110%)',
+          },
+        },
+      },
+      colors: {
+        dark: {
+          primarybg: '#16161a',
+          bg: '#242629',
+          cardbg: '#16161a',
+          headline: '#fffffe',
+          subhead: '#94a1b2',
+          primary: '#7f5af0',
+          primaryhover: '#6647c4',
+          secondary: '#72757e',
+          tertiary: '#2cb67d',
+        },
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
