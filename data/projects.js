1e438b6569cb25c14a13d8e157368e7452693b3d
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-01-30 22:24:19 +0700

    chore: add projects data

---
+++ data/projects.js
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    title: 'Bamboo Commerce',
+    description:
+      'a full-stack e-commerce app with cart, authentication and payment gateway',
+    image: '/img/project-bamboocommerce.gif',
+    link: 'https://bamboocommerce.vercel.app',
+    techs: ['React', 'TailwindCSS', 'Redux', 'ExpressJS', 'MongoDB'],
+  },
+  {
+    title: 'Kreatip',
+    description: 'a landing page for video ads services company',
+    image: '/img/project-kreatip.gif',
+    link: 'https://kreatip-landing.vercel.app',
+    techs: ['React', 'TailwindCSS'],
+  },
+];
